d63938eab9faecf642bf7e65acb86a6f80f71c73
Lex <74861308+GenghisChan1@users.noreply.github.com>
2025-03-27 09:09:18 +0800

    update2

--- YOURANIMELIST/src/main.jsx
+++ YOURANIMELIST/src/main.jsx
@@ -1,12 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { GlobalcontextProvider } from './context/global.jsx'
+import ErrorBoundary from './ErrorBouondary.jsx'
+import { GlobalcontextProvider } from './globalcontext/global.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <GlobalcontextProvider>
-      <App />
-    </GlobalcontextProvider>
+    <ErrorBoundary>
+      <GlobalcontextProvider>
+        <App />
+      </GlobalcontextProvider>
+    </ErrorBoundary>
   </StrictMode>,
 )
